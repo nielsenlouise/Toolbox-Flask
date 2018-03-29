5fdea338a2320242dfeec6d124d73f0289d6e75c
lnielsen <louiseenielsen@gmail.com>
2018-03-29 00:06:30 -0400

    have rendered some templates

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,19 @@
 """
 Put your Flask app code here.
 """
+
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=namez)
+
+if __name__ == '__main__':
+    app.run()
