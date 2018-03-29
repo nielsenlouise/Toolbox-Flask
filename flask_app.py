b2cb94763b2b763d0ce02876c253c9d8be3c04b4
lnielsen <louiseenielsen@gmail.com>
2018-03-29 00:24:13 -0400

    some different names

--- flask_app.py
+++ flask_app.py
@@ -7,13 +7,13 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
-    return render_template('hello.html', name=namez)
+    return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
     app.run()
