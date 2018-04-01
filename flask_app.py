c44c83b0b2d32a5d03a425a39bbd118d360bd844
lnielsen <louiseenielsen@gmail.com>
2018-04-01 12:56:36 -0400

    retains something

--- flask_app.py
+++ flask_app.py
@@ -2,18 +2,26 @@
 Put your Flask app code here.
 """
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, url_for, redirect
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+@app.route('/send_something', methods=['POST'])
+def send_something():
+    submission = request.form['text']
+    print(submission)
+    return redirect(url_for('results', words=submission))
+    # if request.form['text']:
+    #     flash('Yay')
+
+@app.route('/results/<words>')
+def results(words=None):
+    print(words)
+    return render_template('results.html', submission=words)
 
 if __name__ == '__main__':
     app.run()
